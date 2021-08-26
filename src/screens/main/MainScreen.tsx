453dc5423ec8f6a0c53d25d56396b8d9b11b2641
Georgios Kotziabassis <kotziabassis@gmail.com>
2021-08-26 16:12:10 +0300

    ✨ Add tab bar icons

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -3,6 +3,7 @@ import {
   BottomTabNavigationOptions,
   createBottomTabNavigator
 } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/Feather'
 import { MapScreen } from './MapScreen'
 import { SpotsScreen } from './SpotsScreen'
 import { SettingsScreen } from './SettingsScreen'
@@ -17,13 +18,30 @@ const BottomTab = createBottomTabNavigator<BottomTabParamList>()
 
 const options: BottomTabNavigationOptions = {
   tabBarActiveTintColor: '#DB2777',
+  tabBarInactiveTintColor: '#475569',
   headerShown: false
 }
 
+const TabBarIcon = (name: string, focused: boolean): JSX.Element => (
+  <Icon name={name} size={20} color={focused ? '#DB2777' : '#475569'} />
+)
+
 export const MainScreen = (): JSX.Element => (
   <BottomTab.Navigator initialRouteName='Map' screenOptions={options}>
-    <BottomTab.Screen name='Spots' component={SpotsScreen} />
-    <BottomTab.Screen name='Map' component={MapScreen} />
-    <BottomTab.Screen name='Settings' component={SettingsScreen} />
+    <BottomTab.Screen
+      name='Spots'
+      component={SpotsScreen}
+      options={{ tabBarIcon: ({ focused }) => TabBarIcon('map-pin', focused) }}
+    />
+    <BottomTab.Screen
+      name='Map'
+      component={MapScreen}
+      options={{ tabBarIcon: ({ focused }) => TabBarIcon('map', focused) }}
+    />
+    <BottomTab.Screen
+      name='Settings'
+      component={SettingsScreen}
+      options={{ tabBarIcon: ({ focused }) => TabBarIcon('settings', focused) }}
+    />
   </BottomTab.Navigator>
 )
