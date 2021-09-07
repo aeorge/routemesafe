72531f4f265bdcac4e8b20cf4bc52c1498891284
Georgios Kotziabassis <kotziabassis@gmail.com>
2021-09-07 22:34:38 +0200

    ✨ Add initial map

--- src/screens/main/MapScreen.tsx
+++ src/screens/main/MapScreen.tsx
@@ -1,4 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import MapboxGL from '@react-native-mapbox-gl/maps'
 
-export const MapScreen = (): JSX.Element => <View></View>
+MapboxGL.setAccessToken('')
+
+export const MapScreen = (): JSX.Element => (
+  <View style={styles.container}>
+    <MapboxGL.MapView style={styles.map} styleURL={MapboxGL.StyleURL.Light} />
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  }
+})
